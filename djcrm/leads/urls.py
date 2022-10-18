772e9e05c9191fdc41dcd6386520b5e38de0c050
eaaslan <enesalp_aslan@outlook.com>
2022-10-18 15:54:12 -0400

    forms crated

--- djcrm/leads/urls.py
+++ djcrm/leads/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import lead_list
+from .views import lead_create, lead_details, lead_list
 
 app_name = 'leads'
 
 urlpatterns = [
     path("", lead_list),
+    path('<int:pk>',lead_details),
+    path('create/',lead_create),
 ]
--- djcrm/leads/views.py
+++ djcrm/leads/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 
-from .models import Lead
+from urllib import request
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+
+from .forms import LeadForm
+
+from .models import Agent, Lead
 
 
 # Create your views here.
@@ -12,3 +16,42 @@ def lead_list(request):
         'leads': leads
     }
     return render(request, 'leads/lead_list.html', context)
+
+
+def lead_details(request,pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead':lead
+    }
+    return render(request,'leads/lead_details.html',context)
+
+
+def lead_create(request):
+
+ if request.method  == 'POST':
+  form = LeadForm(request.POST)
+  if form.is_valid():
+    first_name = form.cleaned_data['first_name']
+    last_name = form.cleaned_data['last_name']
+    age = form.cleaned_data['age']
+    agent = Agent.objects.first()
+    Lead.objects.create(
+        first_name=first_name,
+        last_name = last_name,
+        age = age,
+        agent = agent
+    )
+    return redirect('/leads')
+
+ else:
+    form = LeadForm()
+    context = {
+        'form':form
+    }
+
+
+    return render(request,'leads/lead_create.html',context)
+     
+
+
+     
